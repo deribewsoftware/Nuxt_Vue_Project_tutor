335a8939f20643f5a6a7fbe98481d285582c2be4
deribew <deribewsoftware@gmail.com>
2024-01-17 16:08:52 +0300

    Card added

---
+++ components/card/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-span-1 flex flex-col gap-2 border-2 bg-white hover:shadow-md rounded-[5px]  m-2">
+    <div class="h-auto w-full">
+    <img src="https://i.pinimg.com/736x/66/d9/f5/66d9f5afdc5337d3f9eac362b970c426.jpg" class="W-full h-full object-cover rounded-t-[5px]"/>
+    </div>
+    <div class="flex w-full justify-between px-4">
+      <h4 class="bg-slate-200 px-2 py-1 font-medium text-sm rounded-full items-center text-center">Villa</h4>
+      <p class="bg-teal-200 px-2 py-1 font-medium rounded-full items-center text-center">12232Etb</p>
+    </div>
+    <div class="flex gap-1">
+      <div class="pl-2 pt-1">
+        <img src="../../public/googlemap.png" class="h-3 w-3"/>
+
+</div>
+<div class="">
+  <p class="text-slate-600 text-sm"> Addis Ababa <span> Gullalle</span> kebele 12</p>
+</div>
+</div>
+
+
+<div class="flex justify-around p-3  border-t-[1.5px] border-slate-300 ">
+  <div class="border-[1.2px] px-2 py-1 border-slate-500 text-sm hover:bg-teal-400 transition  duration-300 rounded-[5px]">Rent</div>
+  <div class="border-[1.2px] px-2 py-1 border-slate-500 text-sm hover:bg-teal-400 transition  duration-300 rounded-[5px]">See Detail</div>
+</div>
+    </div>
+   
+
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,18 @@
   
   <div>
     <Hero/>
-    <div class="grid grid-cols-4 p-4 md:p-10">
-      <div class="bg-rose-100 col-span-1">Serach by any</div>
-      <div class="bg-sky-100 col-span-3">card lists</div>
+    <div class="grid grid-cols-1 md:grid-cols-4 p-4 md:p-10">
+      <div class="hidden md:block bg-white  col-span-1 flex justify-center">
+        <div>Serach by any</div>
+      </div>
+      <div class="col-span-3">
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-10 md:px-20">
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
